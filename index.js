9f29d7712bd576361926a89a53c43020063d0f9a
rajad shakya <rajadshakya@rajads-MacBook-Air.local>
2024-01-20 15:26:44 +0545

    session added

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const session = require("express-session");
 const app = express();
 const { Client } = require("pg");
+
+app.use(
+  session({
+    secret: "ismt123",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const client = new Client({
   user: "postgres",
@@ -22,7 +31,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-  res.render("dashboard");
+  res.render("dashboard", { user: req.session.user });
 });
 
 app.get("/login", (req, res) => {
@@ -34,7 +43,7 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/update", (req, res) => {
-  res.render("update");
+  res.render("update", { user: req.session.user });
 });
 
 app.post("/login", (req, res) => {
@@ -43,6 +52,7 @@ app.post("/login", (req, res) => {
     .then((res_db) => {
       if (res_db.rows.length === 0) return;
       if (res_db.rows[0].password === req.body.password) {
+        req.session.user = res_db.rows[0];
         return res.redirect("/");
       }
     })
@@ -60,7 +70,20 @@ app.post("/register", (req, res) => {
 });
 
 app.post("/update", (req, res) => {
-  res.send("update post");
+  client
+    .query("UPDATE users set email = $1, full_name=$2 where username=$3", [
+      req.body.email,
+      req.body.full_name,
+      req.session.user.username,
+    ])
+    .then((res_db) => {
+      req.session.user.email = req.body.email;
+      req.session.user.full_name = req.body.full_name;
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(3000, (req, res) => {
