3c47b1f510a3fa990c4564bce558c2defe5dab72
rajad shakya <rajadshakya@rajads-MacBook-Air.local>
2024-01-20 15:08:51 +0545

    db code added

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const app = express();
+const { Client } = require("pg");
+
+const client = new Client({
+  user: "postgres",
+  host: "localhost",
+  database: "ismt",
+  password: "inspiron",
+  port: 5432,
+});
+
+client.connect((err) => {
+  if (err) {
+    console.log(err.stack);
+  } else {
+    console.log("Database Connected Sucessfully!!!");
+  }
+});
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded());
@@ -21,11 +38,25 @@ app.get("/update", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.send("login post");
+  client
+    .query("select * from users where username = $1", [req.body.username])
+    .then((res_db) => {
+      if (res_db.rows.length === 0) return;
+      if (res_db.rows[0].password === req.body.password) {
+        return res.redirect("/");
+      }
+    })
+    .catch((err) => console.log("err", err));
 });
 
 app.post("/register", (req, res) => {
-  res.send("register post");
+  client
+    .query(
+      "insert into users(username,password,email,full_name) values ($1,$2,$3,$4)",
+      [req.body.username, req.body.password, req.body.email, req.body.full_name]
+    )
+    .then(() => res.redirect("/login"))
+    .catch((err) => console.log("err", err));
 });
 
 app.post("/update", (req, res) => {
